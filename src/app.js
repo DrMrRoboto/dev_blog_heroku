97d954ccbd6b5940f3a823988d697d57efc79c1e
Ben Margis <margis.ben@gmail.com>
2016-09-09 13:21:57 -0500

    Rebuilding Developer blog for launch on Heroku

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 app = express();
 
+var index = require('../routes/index');
+
+app.use('/',index);
+
 app.get ('/', function (req, res){
-    res.send ('Hello!');
+    res.send ('Hello World!');
 });
 
 var server = app.listen (3000, function() {
